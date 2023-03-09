4e503ba9f51130b33a951fdd4adc616c91f312aa
Vadim Bulitko <bulitko@ualberta.ca>
2023-03-09 12:50:26 -0700

    visualization

--- _empiricalSupport/visualizeChampions.m
+++ _empiricalSupport/visualizeChampions.m
@@ -21,8 +21,8 @@ fn = 'y-2023/results/mrmeGrids_y2023-60x446-14400x352-14179462.mat';
 warning('off','parallel:gpu:device:DeviceDeprecated');
 warning('off','MATLAB:graphics:axestoolbar:PrintWarning');
 
-figMain = figure('Position',[40 40 900 400]);
-tiledlayout(1,2); 
+figMain = figure('Position',[40 40 1200 400]);
+tiledlayout(1,3); 
 
 %% Load the data
 load(fn,'champion');
@@ -32,6 +32,7 @@ load(fn,'champion');
 fprintf('\nMax champion score %d | i = %d\n',maxScore,maxScoreI);
 
 % load the puzzle
+sg = loadPZL2(sprintf('%s/_runWombat/solution.pzl',fnp));
 
 %% Visualize
 % the overall champion
@@ -46,9 +47,26 @@ visualizeHeatMap(champion(maxScoreI).hm);
 title(sprintf('MRME %d | MC %d\nWombat(%d,%s,%0.2f)',champion(maxScoreI).score,champion(maxScoreI).mcscore,...
     champion(maxScoreI).wombat.targetScore,sec2str(champion(maxScoreI).wombat.timeLimit),champion(maxScoreI).wombat.weight));
 
+nexttile(3)
+visualizeGrid(champion(maxScoreI).g,false(size(sg)),sg);
+
 sgtitle(removeUnderscore(fn));
 
-exportgraphics(figMain,sprintf('%s/_runWombat/%s.pdf',fnp,fns));
+exportgraphics(figMain,sprintf('%s/_runWombat/%s-internal.pdf',fnp,fns));
+
+close(figMain);
+
+% figure for the paper
+figMain = figure('Position',[40 40 900 400]);
+tiledlayout(1,2,'TileSpacing','compact','Padding','compact');
+
+nexttile(1)
+visualizeGrid(champion(maxScoreI).g, champion(maxScoreI).p,champion(maxScoreI).cg);
+
+nexttile(2)
+visualizeGrid(champion(maxScoreI).g,false(size(sg)),sg);
+
+exportgraphics(figMain,sprintf('%s/_runWombat/%s-paper.pdf',fnp,fns));
 
 %% Extract the grid
 
